cb5e9433b80014777fa74122383a3dc10f6a82c7
ilyas <ilya_abramov_72@mail.ru>
2024-02-09 15:05:34 +0600

    minor change

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 const express = require("express");
-const taskRoutes =require("./routes/tasks")
+const taskRoutes = require("./routes/tasks");
 const connectDB = require("./config/database");
 const routes = require("./routes/index"); // /index added
-const cookieParser = require("cookie-parser")
-
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = 3000;
@@ -13,26 +13,24 @@ const hostname = "localhost";
 connectDB();
 
 // Serve static files
-app.use('/webpages', express.static(__dirname + '/webpages'));
-app.use('/scripts', express.static(__dirname + '/scripts'));
-app.use('/styles', express.static(__dirname + '/styles'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/webpages", express.static(__dirname + "/webpages"));
+app.use("/scripts", express.static(__dirname + "/scripts"));
+app.use("/styles", express.static(__dirname + "/styles"));
+app.use("/public", express.static(__dirname + "/public"));
 
 // Middleware
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
 //Eto vse lomalo ¯\_(ツ)_/¯
-//app.use(notFound)   
-
-
+//app.use(notFound)
 
 // Use routes
-app.use('/', routes);
-app.use('/api/v1/tasks', taskRoutes);
+app.use("/", routes);
+app.use("/api/v1/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(port, () =>
-    console.log(`App listening at http://${hostname}:${port}`)
+  console.log(`App listening at http://${hostname}:${port}`)
 );
